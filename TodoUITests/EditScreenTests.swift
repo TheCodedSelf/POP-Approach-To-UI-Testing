c45f9ab518a39e7c74996199a99e882b18a16d4c
Keegan Rush <thecodedself@gmail.com>
2018-09-20 18:26:36 +0200

    Create test for editing title

--- TodoUITests/EditScreenTests.swift
+++ TodoUITests/EditScreenTests.swift
@@ -8,8 +8,36 @@
 
 import XCTest
 
-class EditScreenTests: UITestCase, EditScreenStarting {
+class EditScreenTests: UITestCase, EditScreenStarting, TodoListInteracting {
     
     let titleOfTodoForTest = "Grocery Shopping"
 
+    func testCanChangeTitle() {
+        let app = XCUIApplication()
+        
+        // Find the text field with the identifier assigned to Title Field
+        let titleTextField = app.textFields[Accessibility.Edit.TitleField]
+        
+        // Tap to start editing
+        titleTextField.tap()
+        
+        // Clear the previous title
+        titleTextField.clearText()
+        
+        // Type the new title
+        let newTodoTitle = "Attend swift.map"
+        titleTextField.typeText(newTodoTitle)
+        
+        // Tap somewhere else to get rid of the keyboard
+        app.staticTexts[Accessibility.Edit.TitleLabel].tap()
+        
+        // Tap the done button
+        app.buttons[Accessibility.Edit.DoneButton].tap()
+        
+        // Assert that our old todo does not exist
+        XCTAssertFalse(todo(titled: titleOfTodoForTest).exists)
+        
+        // Assert that our new todo does exist
+        XCTAssertTrue(todo(titled: newTodoTitle).exists)
+    }
 }
